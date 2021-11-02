4cfc6aa5161d559e55e87fd2d03dbc62d1ff2c85
avneetgill <avneet2.gill@gmail.com>
2021-11-01 19:50:27 -0600

    structure for app

---
+++ src/components/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Main extends React.Component {
+    
+  
+    render() {
+      return (
+        <div className="LandingPage">
+           Main page
+        </div>
+      );
+    }
+  }
+ 
+  export default Main
